e1690094e0b6beb09059bd54fa8ea220bc36f7a2
rushi5502 <rushikeshathare02@gmail.com>
2024-11-30 15:14:31 +0530

    commit by rushi on 30-11-24
    change in header pricing into about us

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -41,7 +41,7 @@ const Header = () => {
           <div className="md:flex items-center justify-center hidden">
             <MenubarTrigger className="bg-none cursor-pointer">Work</MenubarTrigger>
             <MenubarTrigger className="bg-none cursor-pointer">Services</MenubarTrigger>
-            <MenubarTrigger className="bg-none cursor-pointer">Pricing</MenubarTrigger>
+            <MenubarTrigger className="bg-none cursor-pointer">About Us</MenubarTrigger>
             <MenubarTrigger className="bg-none cursor-pointer">Customer</MenubarTrigger>
             <MenubarTrigger className="bg-none cursor-pointer text-nowrap">Contact Us</MenubarTrigger>
           </div>
@@ -65,7 +65,7 @@ const Header = () => {
                   <div className="md:hidden items-center mt-10 flex flex-col gap-5">
                     <MenubarTrigger className="bg-none cursor-pointer hover:text-orange-500">Work</MenubarTrigger>
                     <MenubarTrigger className="bg-none cursor-pointer hover:text-orange-500">Services</MenubarTrigger>
-                    <MenubarTrigger className="bg-none cursor-pointer hover:text-orange-500">Pricing</MenubarTrigger>
+                    <MenubarTrigger className="bg-none cursor-pointer hover:text-orange-500">About Us</MenubarTrigger>
                     <MenubarTrigger className="bg-none cursor-pointer hover:text-orange-500">Customer</MenubarTrigger>
                     <MenubarTrigger className="bg-none cursor-pointer text-nowrap hover:text-orange-500">Contact Us</MenubarTrigger>
                   </div>
