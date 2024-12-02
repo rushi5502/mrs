7fb7765175ff4e2cad6000fb95dbdbede5cd489c
ShubhamDhansingRajput <163745456+ShubhamDhansingRajput@users.noreply.github.com>
2024-12-02 13:15:54 +0530

    first commit on 02-12-2024

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { Button } from '../ui/button'
  
-import { motion } from "framer-motion";
+import {motion} from "framer-motion"
 import { DotPatternDemo } from '../magicUi/DotPatternDemo';
 const Banner = () => {
   return (
